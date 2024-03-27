02031d1ea3ad0f144c40916a6b1ce0ce70a57038
Adam Benyagoub <yazdraw@gmail..com>
2024-03-27 10:32:30 +0100

    Added buttons to change the current pokemon shown in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,37 @@ import PokemonCard from "./components/PokemonCard";
 import PropTypes from "prop-types";
 
 
-function App() {
-  const pokemonList = [
-    {
+const pokemonList = [
+  {
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
+  
 
 
+
+
+function App() {
 
   // Defining prop types
   PokemonCard.propTypes = {
@@ -29,10 +47,16 @@ function App() {
   }
 
 
+  //States
+  let [pokemonIndex, setPokemonIndex] = useState(0);
+
+
   // Rendering page
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      { ( pokemonIndex <= 0 ) ? "" : <button onClick={() => { setPokemonIndex(pokemonIndex-=1) }}>Precedent</button>}
+      { ( pokemonIndex < pokemonList.length - 1) ? <button onClick={() => { setPokemonIndex(pokemonIndex+=1)}}>Suivant</button> : ""}
     </div>
   )
 }
